c7f429de5d52e53727c268390b18213baa43fa71
YAN Wenkun <code@yanwk.fun>
2024-11-21 15:31:40 +0800

    new project

---
+++ FileHasherAvalonia.Tests/UnitTest.cs
@@ -0,0 +1,15 @@
+namespace FileHasherAvalonia.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
