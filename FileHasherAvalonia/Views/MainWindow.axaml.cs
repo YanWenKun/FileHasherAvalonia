aed0c28424790e936e25209768fd23642d0f27ab
YAN Wenkun <code@yanwk.fun>
2024-11-27 13:25:42 +0800

    dev

--- FileHasherAvalonia/Views/MainWindow.axaml.cs
+++ FileHasherAvalonia/Views/MainWindow.axaml.cs
@@ -1,4 +1,11 @@
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Markup.Xaml;
+using Avalonia.Platform.Storage;
 
 namespace FileHasherAvalonia.Views;
 
